8615757f13bc342ae5c4b6f9951ff76d48f157eb
Erick Lira <erick.s.lira@gmail.com>
2024-05-28 11:28:42 +0100

    Inicial

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController
+{   
+    /* Definindo rota para a classe #Route, com isso vai abrir o "use" acima   */
+    #[Route('/')]
+    public function index(): Response
+    {
+        return new Response('Olá mundo!');
+    }
+
+    /* Definindo rota para a classe #Route, com isso vai abrir o "use" acima   */
+    #[Route('/dinossauro')]
+    public function dinossauro(): Response
+    {
+        return new Response('Olá dinossauro!');
+    }
+}
